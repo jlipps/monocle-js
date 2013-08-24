b88cf1104a402c0d092fb6f5da3d80e3c2f77b0d
Jonathan Lipps <jlipps@gmail.com>
2013-08-23 18:46:41 -0700

    some basic tests passing

--- test/test.js
+++ test/test.js
@@ -4,21 +4,33 @@ var monocle = require('../lib/monocle')
   , launch = monocle.launch
   , run = monocle.run
   , Return = monocle.Return
-  , Callback = monocle.Callback;
+  , Callback = monocle.Callback
+  , should = require('should');
 
 describe('monocle', function() {
+  it('should not reach code after returns', function(done) {
+    var shouldntChange = "foo";
+    var square = o_0(function*(x) {
+      yield new Return(x * x);
+      shouldntChange = "bar";
+    });
+    run(function*() {
+      var s = yield square(3);
+      s.should.equal(9);
+      shouldntChange.should.equal('foo');
+      done();
+    });
+  });
   it('should unwind callbacks', function(done) {
     var f1 = o_0(function*() {
       var cb = new Callback();
-      setTimeout(cb, 500);
-      var res = yield cb;
-      yield new Return(res);
+      setTimeout(cb.handler(), 500);
+      yield cb;
     });
     run(function*() {
       var start = Date.now();
-      var res = yield f1;
-      var end = Date.now();
-      (end - start).should.be.above(499);
+      yield f1();
+      (Date.now() - start).should.be.above(499);
       done();
     });
   });
