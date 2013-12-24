07de2407d73d2a9c8c419fdaefeb62e2dc04d09e
Jonathan Lipps <jlipps@gmail.com>
2013-12-24 14:12:40 -0600

    use ./ for travis

--- build.sh
+++ build.sh
@@ -4,17 +4,17 @@ set -e
 regenerator=$(npm bin)/regenerator
 echo "* Clearing out old es5 lib"
 rm -rf ./lib/es5
-mkdir lib/es5
+mkdir ./lib/es5
 echo "* Regenerating new es5 lib"
-$regenerator -r lib/monocle.js > lib/es5/monocle.js
-sed -i '' -e 's/harmony \= true/harmony = false/' lib/es5/monocle.js
-$regenerator lib/utils.js > lib/es5/utils.js
-cp lib/callback.js lib/es5/callback.js
-cp lib/helpers.js lib/es5/helpers.js
+$regenerator -r ./lib/monocle.js > ./lib/es5/monocle.js
+sed -i '' -e 's/harmony \= true/harmony = false/' ./lib/es5/monocle.js
+$regenerator ./lib/utils.js > ./lib/es5/utils.js
+cp ./lib/callback.js ./lib/es5/callback.js
+cp ./lib/helpers.js ./lib/es5/helpers.js
 
 echo "* Clearing out old es5 tests"
-rm -rf test/es5
-mkdir test/es5
+rm -rf ./test/es5
+mkdir ./test/es5
 echo "* Regenerating new es5 tests"
-$regenerator test/es6/tests.js > test/es5/tests.js
-sed -i '' -e 's/harmony \= true/harmony = false/' test/es5/tests.js
+$regenerator ./test/es6/tests.js > ./test/es5/tests.js
+sed -i '' -e 's/harmony \= true/harmony = false/' ./test/es5/tests.js
